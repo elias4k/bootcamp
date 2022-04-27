f1583431c635f07d9150d63d06b0a19fd62f4bbc
elias4k <40409522+elias4k@users.noreply.github.com>
2022-04-27 00:23:55 -0300

    .

--- EjerciciosClase3y4/EjerciciosClase3y4/Cuadrado.cs
+++ EjerciciosClase3y4/EjerciciosClase3y4/Cuadrado.cs
@@ -8,5 +8,18 @@ namespace EjerciciosClase3y4
 {
     internal class Cuadrado
     {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public Cuadrado(double BaseCuadrado, double AlturaCuadrado = 0)
+        {
+            this.Base = BaseCuadrado;
+            this.Altura = AlturaCuadrado > 0 ? AlturaCuadrado : this.Base;
+            return;
+        }
+        public float Area()
+        {
+            return (float)(Base * Altura);
+        }
     }
 }
--- EjerciciosClase3y4/EjerciciosClase3y4/Program.cs
+++ EjerciciosClase3y4/EjerciciosClase3y4/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EjerciciosClase3y4;
+
 Console.WriteLine("Hello, World!");
+var cuadrado = new Cuadrado(2.0);
+Console.WriteLine(cuadrado.Area());
+var cuadrado2 = new Cuadrado(2.0, 3);
+Console.WriteLine(cuadrado2.Area());
