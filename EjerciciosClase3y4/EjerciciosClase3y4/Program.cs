c55da22dc56e7a0dda8bc27ce0fe15acb2f9d232
elias4k <40409522+elias4k@users.noreply.github.com>
2022-04-27 10:48:15 -0300

    Edad method

--- EjerciciosClase3y4/EjerciciosClase3y4/Program.cs
+++ EjerciciosClase3y4/EjerciciosClase3y4/Program.cs
@@ -5,3 +5,6 @@ var cuadrado = new Cuadrado(2.5);
 Console.WriteLine(cuadrado.AreaToString());
 var cuadrado2 = new Cuadrado(2.5, 3);
 Console.WriteLine(cuadrado2.AreaToString());
+
+var persona = new Persona("Elias", "Lezcano", "1997-05-22");
+Console.WriteLine(persona.EdadToString());
