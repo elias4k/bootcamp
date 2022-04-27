e85fd96535e48d21204d12b714c8137de57ab120
elias4k <40409522+elias4k@users.noreply.github.com>
2022-04-27 10:48:15 -0300

    Edad method

--- EjerciciosClase3y4/EjerciciosClase3y4/Persona.cs
+++ EjerciciosClase3y4/EjerciciosClase3y4/Persona.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace EjerciciosClase3y4
 {
-    internal class Persona
+    class Persona
     {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateOnly FechaNac { get; set; }
+
+        public Persona(string Nombre, string Apellido, string FechaNacimiento)
+        {
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            if (FechaNacimiento != null)
+            {
+                var fecha = FechaNacimiento.Split("-");
+                this.FechaNac = new DateOnly(
+                    Int32.Parse(fecha[0]),
+                    Int32.Parse(fecha[1]),
+                    Int32.Parse(fecha[2])
+                    );
+            }
+            
+        }
+
+        public string NombreCompleto()
+        {
+            return Nombre + " " + Apellido;
+        }
+
+        public int Edad(bool YaCumplio = true)
+        {
+            int edad = 2022 - this.FechaNac.Year;
+            if (!YaCumplio)
+                edad--;
+            return edad;
+        }
+
+        public string EdadToString()
+        {
+            return "Tiene " + Edad() + " Años";
+        }
+
+
     }
 }
