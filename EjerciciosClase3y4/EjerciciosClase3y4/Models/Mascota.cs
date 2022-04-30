b1333a3302654a565acbf85e62b997b2ea5d1330
elias4k <40409522+elias4k@users.noreply.github.com>
2022-04-30 19:59:51 -0300

    Create Mascota.cs

---
+++ EjerciciosClase3y4/EjerciciosClase3y4/Models/Mascota.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosClase3y4.Models
+{
+    internal class Mascota
+    {
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public int Energia { get; set; }
+
+        public Mascota(string Nombre, string Tipo)
+        {
+            this.Nombre = Nombre;   
+            this.Tipo = Tipo;
+            Energia = 0;
+        }
+
+        public void Alimentar()
+        {
+            this.Energia = +30;
+            if (this.Energia > 100)
+                this.Energia = 100;
+
+        }
+
+        public void Correr()
+        {
+            if (this.Energia > 20)
+            {
+                this.Energia -= 10;
+                if (this.Energia < 0)
+                    this.Energia = 0;
+                return;
+            }
+            else
+            {
+                Console.WriteLine(Nombre + " está muy cansado.");
+                return;
+            }
+        }
+    }
+}
