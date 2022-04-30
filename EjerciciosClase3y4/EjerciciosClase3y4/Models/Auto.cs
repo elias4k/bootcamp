6027cba819ef1b44ad1fbe0250cc9da6c289f8e7
elias4k <40409522+elias4k@users.noreply.github.com>
2022-04-30 17:39:23 -0300

    Create Auto.cs

---
+++ EjerciciosClase3y4/EjerciciosClase3y4/Models/Auto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosClase3y4.Models
+{
+    class Auto
+    {
+        public bool Encendido { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public string Color { get; set; }
+
+        public Auto(string Marca, string Modelo, string Color)
+        {
+            this.Marca = Marca; 
+            this.Modelo = Modelo;
+            this.Color = Color;
+            this.Encendido = false;
+            return;
+        }
+
+        public void Encender()
+        {
+            this.Encendido = true;
+        }
+
+        public void Apagar()
+        {
+            this.Encendido = false;
+        }
+    }
+}
