ed862fb5f72b230360b555504d8ad799945e9dac
Elias Lezcano <elias4k@gmail.com>
2022-05-08 02:11:27 -0300

    C4 E3

--- Actividades/Clase4/Ejercicios/Lista.cs
+++ Actividades/Clase4/Ejercicios/Lista.cs
@@ -8,7 +8,7 @@ namespace Ejercicios
 {
     class Lista
     {
-        public static double ejecrcicio1()
+        public static double ejercicio1()
         {
             var notas = new List<int>() { 6, 3, 7, 7, 9, 10, 8, 8, 6, 10 };
             double total = 0;
@@ -18,7 +18,7 @@ namespace Ejercicios
             }
             return total / notas.Count;
         }
-        public static string ejecrcicio2()
+        public static string ejercicio2()
         {
             var edades = new List<int>() { 16, 33, 17, 27, 9, 10, 28, 18, 96, 12 };
             int mayores = 0;
@@ -31,6 +31,20 @@ namespace Ejercicios
                     menores = +1;
             }
             return "Cantidad de mayores: " + mayores + "\nCantidad de menores: " + menores;
+        }
+        public static string ejercicio3()
+        {
+            var nombres = new List<string>() { "Juan", "Pedro", "Alejando", "Alberto", "Ignacio" };
+            string mayor = "";
+            string menor = "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss";
+            foreach(string nombre in nombres)
+            {
+                if (nombre.Length > mayor.Length)
+                    mayor = nombre;
+                if (nombre.Length < menor.Length)
+                    menor = nombre;
+            }
+            return "Nombre mas largo: " + mayor + " con "+ mayor.Length+" caracteres\nNombre mas corto: " + menor + " con " + menor.Length + " caracteres";
         }
     }
 }
--- Actividades/Clase4/Ejercicios/Program.cs
+++ Actividades/Clase4/Ejercicios/Program.cs
@@ -2,9 +2,12 @@
 
 
 Console.WriteLine("\n\n*** Ejercicio 1 ***");
-Console.WriteLine("El promedio de las notas de los alumnos es: " + Lista.ejecrcicio1());
+Console.WriteLine("El promedio de las notas de los alumnos es: " + Lista.ejercicio1());
 
 
 Console.WriteLine("\n\n*** Ejercicio 2 ***");
-Console.WriteLine(Lista.ejecrcicio2());
+Console.WriteLine(Lista.ejercicio2());
 
+
+Console.WriteLine("\n\n*** Ejercicio 3 ***");
+Console.WriteLine(Lista.ejercicio3());
