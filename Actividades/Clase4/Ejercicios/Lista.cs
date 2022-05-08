7559a1af51d0b04272929c682b4fd555264051ce
Elias Lezcano <elias4k@gmail.com>
2022-05-08 01:46:22 -0300

    C4 E2

--- Actividades/Clase4/Ejercicios/Lista.cs
+++ Actividades/Clase4/Ejercicios/Lista.cs
@@ -10,13 +10,27 @@ namespace Ejercicios
     {
         public static double ejecrcicio1()
         {
-            var notas = new List<int>(){ 6, 3, 7, 7, 9, 10, 8, 8, 6, 10 };
+            var notas = new List<int>() { 6, 3, 7, 7, 9, 10, 8, 8, 6, 10 };
             double total = 0;
             foreach (int i in notas)
             {
                 total = total + i;
             }
             return total / notas.Count;
+        }
+        public static string ejecrcicio2()
+        {
+            var edades = new List<int>() { 16, 33, 17, 27, 9, 10, 28, 18, 96, 12 };
+            int mayores = 0;
+            int menores = 0;
+            foreach (int edad in edades)
+            {
+                if (edad >= 18)
+                    mayores = +1;
+                else
+                    menores = +1;
+            }
+            return "Cantidad de mayores: " + mayores + "\nCantidad de menores: " + menores;
         }
     }
 }
--- Actividades/Clase4/Ejercicios/Program.cs
+++ Actividades/Clase4/Ejercicios/Program.cs
@@ -1,4 +1,10 @@
 ﻿using Ejercicios;
 
-// Ejercicio 1
+
+Console.WriteLine("\n\n*** Ejercicio 1 ***");
 Console.WriteLine("El promedio de las notas de los alumnos es: " + Lista.ejecrcicio1());
+
+
+Console.WriteLine("\n\n*** Ejercicio 2 ***");
+Console.WriteLine(Lista.ejecrcicio2());
+
