69d779bb83cbbd316a7d200815cc2844f42afdf1
elias4k <40409522+elias4k@users.noreply.github.com>
2022-05-27 13:02:19 -0300

    Create Pet.cs
    Model

---
+++ WebApi-Project/WebApi/Models/Pet.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models
+{ 
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Breed { get; set; }
+    }
+}
